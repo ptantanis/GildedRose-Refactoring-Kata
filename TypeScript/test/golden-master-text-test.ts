7969def260a8257b91dffd9b2c9553b39239fbfc
Pachara Tantanis <p.tantanis@live.com>
2023-04-11 18:02:35 +0700

    fix texttest for typescript

--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -20,11 +20,13 @@ if (process.argv.length > 2) {
     days = +process.argv[2];
   }
 
-for (let i = 0; i < days; i++) {
+console.log("OMGHAI!");
+
+for (let i = 0; i <= days; i++) {
   console.log("-------- day " + i + " --------");
   console.log("name, sellIn, quality");
   items.forEach(element => {
-    console.log(element.name + ' ' + element.sellIn + ' ' + element.quality);
+    console.log(element.name + ', ' + element.sellIn + ', ' + element.quality);
 
   });
   console.log();
