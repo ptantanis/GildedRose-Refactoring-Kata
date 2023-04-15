7f5812bf4944ac4b8979a95000724614a72616cd
Pachara Tantanis <p.tantanis@live.com>
2023-04-15 17:39:17 +0700

    group update quality logic together

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -13,6 +13,12 @@ export class GildedRose {
         if (item.quality < 50) {
           item.quality = item.quality + 1
         }
+
+        if (item.sellIn <= 0) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1
+          }
+        }
       } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality < 50) {
           item.quality = item.quality + 1
@@ -28,26 +34,19 @@ export class GildedRose {
             item.quality = item.quality + 1
           }
         }
+
+        if (item.sellIn <= 0) {
+          item.quality = 0
+        }
       } else if (item.name == 'Sulfuras, Hand of Ragnaros') {
       } else {
         if (item.quality > 0) {
           item.quality = item.quality - 1
         }
-      }
 
-      if (item.sellIn <= 0) {
-        if (item.name == 'Aged Brie') {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
-        } else {
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            item.quality = 0
-          } else if (item.name == 'Sulfuras, Hand of Ragnaros') {
-          } else {
-            if (item.quality > 0) {
-              item.quality = item.quality - 1
-            }
+        if (item.sellIn <= 0) {
+          if (item.quality > 0) {
+            item.quality = item.quality - 1
           }
         }
       }
