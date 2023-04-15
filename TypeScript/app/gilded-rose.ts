1404c4497b7f1414094c4d9216671ae1372b9383
Pachara Tantanis <p.tantanis@live.com>
2023-04-15 18:08:42 +0700

    add calculate item quality function

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -14,43 +14,29 @@ export class GildedRose {
       }
 
       if (item.name == 'Aged Brie') {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
+        item.quality = this.calculateItemQuality(item.quality, 1)
 
         if (item.sellIn <= 0) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
+          item.quality = this.calculateItemQuality(item.quality, 1)
         }
       } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
+        item.quality = this.calculateItemQuality(item.quality, 1)
 
         if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
+          item.quality = this.calculateItemQuality(item.quality, 1)
         }
         if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
+          item.quality = this.calculateItemQuality(item.quality, 1)
         }
 
         if (item.sellIn <= 0) {
           item.quality = 0
         }
       } else {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1
-        }
+        item.quality = this.calculateItemQuality(item.quality, -1)
 
         if (item.sellIn <= 0) {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1
-          }
+          item.quality = this.calculateItemQuality(item.quality, -1)
         }
       }
 
@@ -58,5 +44,18 @@ export class GildedRose {
     }
 
     return this.items;
+  }
+
+  private calculateItemQuality(baseQuality: number, adjustQuality: number): number {
+    const newQuality = baseQuality + adjustQuality
+    if (newQuality < 0) {
+      return 0
+    }
+
+    if (newQuality > 50) {
+      return 50
+    }
+
+    return newQuality
   }
 }
